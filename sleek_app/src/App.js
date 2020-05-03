bb4b1789fe3e6d4ae287992bfba6bc78826a7193
ralph <ralphfleyga@gmail.com>
2020-05-03 16:29:00 +0800

    update App.js

--- sleek_app/src/App.js
+++ sleek_app/src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom'
+import history from './history'
+import { connect } from 'react-redux'
 
-function App() {
+import Home from './components/layout/Home'
+import Header from './components/layout/Header'
+import Login from './components/Login'
+import Feeds from './components/workspaces/Feeds'
+import Explore from './components/workspaces/Explore'
+import Teams from './components/workspaces/Teams'
+
+
+const PrivateRoutes = ({ children, ...rest }) => {
+  // redirect to login
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      localStorage.getItem('isLoggedIn') ? (
+              children
+        ) : ("")
+  )
 }
 
-export default App;
+
+class App extends Component {
+
+  render () {
+    return (
+      
+        <Router history={history}>
+            <Header />
+            <div className="container">
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/login/' component={Login} />
+
+                <PrivateRoutes>
+                  <Route exact path='/feeds/' component={Feeds} />
+                  <Route exact path='/explore/' component={Explore} />
+                  <Route exact path='/teams/' component={Teams} />
+                  <Route exact path="/logout/">
+                      {this.props.auth.isLoggedIn ?<Home /> : <Redirect to="/login/" />}
+                  </Route>
+                </PrivateRoutes>
+              </Switch>
+            </div>
+        </Router>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+      auth: state.auth,
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
